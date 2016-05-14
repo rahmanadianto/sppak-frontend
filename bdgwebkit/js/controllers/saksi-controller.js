440e11317ba9cb6fdc3870bc8afb207ebf0c9aa4
= <octavianusmarcel12@gmail.com>
2016-05-14 12:09:43 +0700

    Verification on Saski done

--- bdgwebkit/js/controllers/saksi-controller.js
+++ bdgwebkit/js/controllers/saksi-controller.js
@@ -17,15 +17,51 @@ app.controller('SaksiCtrl', function($anchorScroll, $location, $http, $rootScope
     }
     
     var getId = function() {
-		return SaksiService.getId();
+		var id = getUrlParameter('pid');
+		return id;
+	}
+	
+	// Taken from http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html with modification
+	var getUrlParameter = function getUrlParameter(sParam) {
+		var sPageURL = window.location.href.split('?')[1];
+		var sURLVariables = sPageURL.split('&');
+		var sParameterName;
+		var i;
+
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+		}
 	}
 	
     var scrollToTop = function() {
         $location.hash('content');
         $anchorScroll();
     }
+    
+    var verifikasi = function(){
+		var sPageURL = window.location.href;
+		console.log(sPageURL);
+		var token = getUrlParameter('token');
+		var saksiId = getUrlParameter('id');
+		SaksiService.verifikasi(saksiId, token)
+		.then(function(res) {
+			$scope.success = true;
+		})
+		.catch(function(err) {
+			if (err) {
+				alert(err.message);
+			} else {
+				alert("Terjadi error pada server. Mohon maaf.");
+			}
+		});
+	}
 	
 	$scope.getKelahiran = getKelahiran(getId());
 	$scope.scrollToTop = scrollToTop;
+	$scope.verifikasi = verifikasi;
 });
 
--- bdgwebkit/js/services/saksi-service.js
+++ bdgwebkit/js/services/saksi-service.js
@@ -1,9 +1,29 @@
 app.factory('SaksiService', function($http, $q) {
     var factory = {};
+    var saksiEndpoint = 'http://localhost:8000/api/v1/saksi/';
     
-    factory.getId = function() {
-		return 1;
+    var getEndpoint = function(saksiId, token){
+		return saksiEndpoint + saksiId + '/verifikasi/' + decodeURIComponent(token);
 	}
+	
+    factory.verifikasi = function(saksiId, token){
+		var deferred = $q.defer();
+		var endpoint = getEndpoint(saksiId, token);
+		console.log(endpoint);
+		
+        $http({
+            method: 'GET',
+            url: endpoint,
+        }).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(data) {
+            deferred.reject(data);
+        });
+
+        return deferred.promise;	
+	}
+	
+	
 	
     return factory;
 });
